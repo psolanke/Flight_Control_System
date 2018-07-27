c5970c7111f202659f12744aa099e5e2b5726107
psolanke <pratiksolankes@gmail.com>
2018-07-27 17:34:36 +0530

    Hurry commit

--- Ground_Station/GroundStation/loginPage/urls.py
+++ Ground_Station/GroundStation/loginPage/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'loginPage'
 urlpatterns = [
-    path(r'', views.loginPage, name='loginPage'),
+    path(r'', views.LoginPage.as_view(), name='loginPage'),
     
 ]
--- Ground_Station/GroundStation/loginPage/views.py
+++ Ground_Station/GroundStation/loginPage/views.py
@@ -1,38 +1,49 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from django.views import View
 from .forms import NameForm
 import requests
 from . import strings
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def loginPage(request):
-    if request.method == strings.POST_METHOD:
-        form = NameForm(request.POST)
-        if form.is_valid():
-            response_dict = connect(form.cleaned_data['url'])
+class LoginPage(APIView):
+    def post(self, request):
+        # form = NameForm(request.POST)
+        # if form.is_valid():
+        request.POST[]
+            response_dict = self.connect(form.cleaned_data['url'])
             if response_dict[strings.SUCCESS_KEY] == True:
                 request.session['url'] = form.cleaned_data['url']
                 request.session['is_connected'] = True
                 request.session['global_namespace'] = response_dict['param_info']['param_value']
                 return HttpResponseRedirect('/gs')
-        success = strings.CONNECTION_ERROR_MESSAGE
-    else:
-        form = NameForm()
-        success = None
-    context = {
-        strings.FORM_KEY : form,
-        strings.TITLE_KEY : strings.LOGIN_PAGE_TITLE,
-        strings.SUCCESS_KEY : success
-    }
-    return render(request, strings.LOGINPAGE_TEMPLATE, context)
+        success = strings.CONNECTION_ERROR_MESSAGE            
+        context = {
+            strings.FORM_KEY : form,
+            strings.TITLE_KEY : strings.LOGIN_PAGE_TITLE,
+            strings.SUCCESS_KEY : success
+        }
+        return Response(context)
+        # return render(request, strings.LOGINPAGE_TEMPLATE, context)
+    # def get(self, request):
+    #     form = NameForm()
+    #     success = None
+    #     context = {
+    #         strings.FORM_KEY : form,
+    #         strings.TITLE_KEY : strings.LOGIN_PAGE_TITLE,
+    #         strings.SUCCESS_KEY : success
+    #     }
+    #     return render(request, strings.LOGINPAGE_TEMPLATE, context)
 
-def connect(url):
-    try:
-        response = requests.get(strings.GLOBAL_NAMESPACE_URL.format(url))
-        response_dict = response.json()
-    except:
-        response_dict = {strings.SUCCESS_KEY : False}
-    return response_dict
+    def connect(self, url):
+        try:
+            response = requests.get(strings.GLOBAL_NAMESPACE_URL.format(url))
+            response_dict = response.json()
+        except:
+            response_dict = {strings.SUCCESS_KEY : False}
+        return response_dict
+    
 
 
 
